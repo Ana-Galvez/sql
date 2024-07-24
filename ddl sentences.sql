0db2d5fba512a17637e27e7fef140f44344c4657
Ana Galvez <anamariagalvez01@gmail.com>
2024-07-24 16:23:56 -0300

    refactor: by writing sql sentences

---
+++ ddl sentences.sql
@@ -0,0 +1,8 @@
+create database if not exists mis_familiares;
+use mis_familiares;
+create table if not exists mi_familiar(
+	id_familiar int not null auto_increment,
+    nombre varchar(20) not null,
+    apellido varchar(20) not null,
+    primary key(id_familiar)
+);
