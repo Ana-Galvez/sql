3cd52ca53db7d36ca2f35551240dd2512ce97888
Ana Galvez <anamariagalvez01@gmail.com>
2024-08-03 17:34:34 -0300

    refactor: part8 ddl sentences

--- ddl sentences.sql
+++ ddl sentences.sql
@@ -1,6 +1,6 @@
 create database if not exists mis_familiares;
 use mis_familiares;
-create table if not exists mi_familiar(
+create table if not exists familiares(
 	id_familiar int not null auto_increment,
     nombre varchar(20) not null,
     apellido varchar(20) not null,
@@ -9,21 +9,22 @@ create table if not exists mi_familiar(
 
 show databases;
 show tables;
+describe familiares;
 
-alter table mi_familiar rename to familiar;
+alter table familiares rename to mis_otros_familiares;
 
-alter table familiar add column estatura float not null;
-alter table familiar add column fecha_nacimiento date not null;
+alter table mis_otros_familiares add column estatura float unsigned not null;
+alter table mis_otros_familiares add column fecha_nacimiento date not null;
 
-alter table familiar change column nombre nombre varchar(40) not null;
-alter table familiar change column apellido apellido varchar(40) not null;
+alter table mis_otros_familiares change column nombre nombre varchar(40) not null;
+alter table mis_otros_familiares change column apellido apellido varchar(40) not null;
 
-alter table familiar rename column fecha_nacimiento to fecha_de_nacimiento;
+alter table mis_otros_familiares rename column fecha_nacimiento to fecha_de_nacimiento;
 
-alter table familiar drop column estatura;
-alter table familiar drop column fecha_de_nacimiento;
-alter table familiar drop column apellido;
-alter table familiar drop column nombre;
+alter table mis_otros_familiares drop column estatura;
+alter table mis_otros_familiares drop column fecha_de_nacimiento;
+alter table mis_otros_familiares drop column apellido;
+alter table mis_otros_familiares drop column nombre;
 
-drop table if exists familiar;
+drop table if exists mis_otros_familiares;
 drop database if exists mis_familiares;
